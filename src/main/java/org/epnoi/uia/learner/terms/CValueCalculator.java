f6777bc0cf1be500c5e6954190d3bbc9108c2869
Rafael Gonzalez Cabero <rafael.gonzalez.cabero@gmail.com>
2014-06-09 17:36:41 +0200

    Some files related with the CValue calculation have been added

---
+++ src/main/java/org/epnoi/uia/learner/terms/CValueCalculator.java
@@ -0,0 +1,16 @@
+package org.epnoi.uia.learner.terms;
+
+import java.util.HashMap;
+
+public class CValueCalculator {
+	TermsIndex terms;
+
+	public CValueCalculator() {
+		this.terms = new TermsIndex();
+	}
+
+	public static void main(String[] args) {
+
+	}
+
+}
