427ad59c8bc2c52b620c5d843ab9873fec1b5aca
Rafael Gonzalez Cabero <rafael.gonzalez.cabero@gmail.com>
2015-01-12 00:32:00 +0100

    Fixes #64 : The curated relations table creator has been implemented (created using wordnet, will be enhanced with wikidata info soon)

--- src/main/java/org/epnoi/uia/learner/nlp/wordnet/WordNetHandler.java
+++ src/main/java/org/epnoi/uia/learner/nlp/wordnet/WordNetHandler.java
@@ -2,6 +2,7 @@ package org.epnoi.uia.learner.nlp.wordnet;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.Iterator;
 import java.util.List;
@@ -78,7 +79,7 @@ public class WordNetHandler {
 			List<IWord> words;
 			for (ISynsetID sid : hypernyms) {
 				words = this.wordNetDictionary.getSynset(sid).getWords();
-				//System.out.print(sid + " {");
+				// System.out.print(sid + " {");
 				for (Iterator<IWord> i = words.iterator(); i.hasNext();) {
 
 					nounHypernyms.add(i.next().getLemma());
@@ -89,18 +90,39 @@ public class WordNetHandler {
 		return nounHypernyms;
 
 	}
-	
+
 	// ---------------------------------------------------------------------------------------------------------------
 
-	public List<String> stemNoun(String noun) {
-		return this.wordnetStemmer.findStems(noun, POS.NOUN);
+	public String stemNoun(String noun) {
+		List<String> stemmerResult = this.wordnetStemmer.findStems(noun,
+				POS.NOUN);
+		if (stemmerResult.size() > 0) {
+			return this.wordnetStemmer.findStems(noun, POS.NOUN).get(0);
+		} else {
+			return null;
+		}
+
 	}
-	
+
+	// ---------------------------------------------------------------------------------------------------------------
+
+	public List<String> getNouns() {
+		List<String> nouns = new ArrayList<String>();
+		for (Iterator<IIndexWord> i = this.wordNetDictionary
+				.getIndexWordIterator(POS.NOUN); i.hasNext();) {
+			IIndexWord wid = i.next();
+			nouns.add(wid.getLemma());
+		}
+		return nouns;
+	}
+
 	// ---------------------------------------------------------------------------------------------------------------
 
 	public static void main(String[] args) {
 		System.out.println("Starting WordNetHandlerTest");
-		String filepath = "/epnoi/epnoideployment/wordnet/dictWN3.1/";
+		// String filepath = "/epnoi/epnoideployment/wordnet/dictWN3.1/";
+
+		String filepath = "/epnoi/epnoideployment/wordnet/dictWN40K/dict";
 		WordNetParameters parameters = new WordNetParameters();
 		parameters
 				.setParameter(WordNetParameters.DICTIONARY_LOCATION, filepath);
@@ -126,7 +148,31 @@ public class WordNetHandler {
 				.println("Testing for lions--------------------------------------------------------");
 
 		System.out.println(handler.getNounFirstMeaningHypernyms("lions"));
+
+		System.out.println("Testing the get nouns function");
+		long t = System.currentTimeMillis();
+
+		System.out.println("These are the nouns ");
+		int count = 0;
+		int hypernymsTotal = 0;
+		List<String> nouns = handler.getNouns();
+		for (String noun : nouns) {
+			Set<String> hypernyms = handler.getNounFirstMeaningHypernyms(noun);
+			System.out.println(":::> " + noun + " -> " + hypernyms);
+			if (hypernyms.size() > 0) {
+				count = count + 1;
+				hypernymsTotal += hypernyms.size();
+			}
+		}
+		System.out.println("There are " + nouns.size() + " nouns");
+		System.out.println("About " + ((double) count)
+				/ ((double) nouns.size()) + " have hypernyms defined");
+		System.out.println("With an average of " + ((double) hypernymsTotal)
+				/ ((double) nouns.size()) + "hypernyms each");
+		long time = System.currentTimeMillis() - t;
+		System.out.printf(" done in " + String.valueOf(time));
 		System.out.println("Ending WordNetHandlerTest");
 
 	}
+
 }
---
+++ src/main/java/org/epnoi/uia/learner/relations/CuratedRelationsTableCreator.java
@@ -0,0 +1,69 @@
+package org.epnoi.uia.learner.relations;
+
+import org.epnoi.uia.exceptions.EpnoiInitializationException;
+import org.epnoi.uia.learner.nlp.wordnet.WordNetHandler;
+import org.epnoi.uia.learner.nlp.wordnet.WordNetParameters;
+
+public class CuratedRelationsTableCreator {
+	private WordNetHandler wordnetHandler;
+
+	// ------------------------------------------------------------------------------------
+
+	public void init(WordNetParameters parameters)
+			throws EpnoiInitializationException {
+		this.wordnetHandler = new WordNetHandler();
+		this.wordnetHandler.init(parameters);
+	}
+
+	// ------------------------------------------------------------------------------------
+
+	public CuratedRelationsTable build() {
+		CuratedRelationsTable curatedRealtionsTable = new CuratedRelationsTable(
+				this.wordnetHandler);
+		for (String noun : wordnetHandler.getNouns()) {
+			/*
+			 * Set<String> nounHypernyms = wordnetHandler
+			 * .getNounFirstMeaningHypernyms(noun);
+			 */
+			curatedRealtionsTable.addHypernym(noun,
+					wordnetHandler.getNounFirstMeaningHypernyms(noun));
+
+		}
+		return curatedRealtionsTable;
+	}
+
+	// ------------------------------------------------------------------------------------
+
+	public static void main(String[] args) {
+		System.out.println("Starting the CuratedRelationsTableCreator test!!");
+
+		String filepath = "/epnoi/epnoideployment/wordnet/dictWN3.1/";
+		WordNetParameters parameters = new WordNetParameters();
+		parameters
+				.setParameter(WordNetParameters.DICTIONARY_LOCATION, filepath);
+
+		CuratedRelationsTableCreator curatedRelationsTableCreator = new CuratedRelationsTableCreator();
+		try {
+			curatedRelationsTableCreator.init(parameters);
+		} catch (EpnoiInitializationException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		CuratedRelationsTable curatedRelationsTable = curatedRelationsTableCreator
+				.build();
+		System.out
+				.println("Testing for dog-canine-------------------------------------------------------");
+		System.out.println(curatedRelationsTable.areRelated("dog", "canrine"));
+
+		System.out
+				.println("Testing for dogs-canine-------------------------------------------------------");
+		System.out.println(curatedRelationsTable.areRelated("dogs", "canine"));
+
+		System.out
+				.println("Testing for dog-canines-------------------------------------------------------");
+		System.out.println(curatedRelationsTable.areRelated("dog", "canines "));
+
+		System.out.println("Starting the CuratedRelationsTableCreator test!!");
+	}
+
+}
