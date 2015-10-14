17b3c12d5afa480160e6261f2f5c404d35792650
Carlos Badenes <cbadenes@gmail.com>
2015-10-14 17:06:02 +0200

    get method for message

--- model/src/main/java/org/epnoi/model/Event.java
+++ model/src/main/java/org/epnoi/model/Event.java
@@ -1,15 +1,14 @@
 package org.epnoi.model;
 
+import lombok.Data;
+
+@Data
 public class Event {
+
 	private String message = null;
 
 	public Event(String message) {
 		this.message = message;
-	}
-
-	@Override
-	public String toString() {
-		return "Event [message=" + message + "]";
 	}
 
 }
