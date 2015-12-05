aa6821483a1a0903b6d7996cac677ec775b88cfc
fitash <rafael.gonzalez.cabero@gmail.com>
2015-12-05 22:49:02 +0100

    The CoreTest has been removed :'(

--- core/src/test/java/org/epnoi/CoreTest.java
+++ core/src/test/java/org/epnoi/CoreTest.java
@@ -12,11 +12,12 @@ import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 /**
  * Created by rgonzalez on 3/12/15.
  */
-
+/*COMMENTED SINCE NEEDS A LOT OF LOCAL DATA
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration(classes = org.epnoi.EpnoiConfig.class)
 @ActiveProfiles("develop")
 @TestPropertySource(properties = { "epnoi.config.path = testUIA.xml" })
+*/
 public class CoreTest {
 
 
