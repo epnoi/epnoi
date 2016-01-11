328f4d05c2097e147416ddf0022880fc60e9e210
Carlos Badenes <cbadenes@gmail.com>
2016-01-11 11:57:45 +0100

    removed slf4j-log4j12 library as transitive dependency. It should be only defined in the final module

--- storage/src/main/java/org/epnoi/storage/model/Source.java
+++ storage/src/main/java/org/epnoi/storage/model/Source.java
@@ -1,7 +1,9 @@
 package org.epnoi.storage.model;
 
+import com.google.common.base.Strings;
 import lombok.Data;
 import lombok.ToString;
+import org.apache.commons.lang.StringUtils;
 
 /**
  * Created by cbadenes on 22/12/15.
@@ -19,4 +21,22 @@ public class Source extends Resource {
     private String protocol;
 
     private String domain;
+
+    public String getProtocol(){
+        if (Strings.isNullOrEmpty(protocol)){
+            return StringUtils.substringBefore(url,":");
+        }
+        return protocol;
+    }
+
+    public String getName(){
+        if (Strings.isNullOrEmpty(name)){
+            return StringUtils.substringBetween(url,"//","/");
+        }
+        return name;
+    }
+
+    public String getServer(){
+        return StringUtils.substringBefore(url, "?");
+    }
 }
